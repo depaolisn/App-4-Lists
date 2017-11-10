aa0d59a2a7cc1d882599d7d31362891006094a16
Shawn Toubeau <shawntoubeau@gmail.com>
2017-11-09 21:05:34 -0500

    Added content to Player and PlayerHand classes

--- src/edu/wit/dcsn/comp2000/listapp/Player.java
+++ src/edu/wit/dcsn/comp2000/listapp/Player.java
@@ -1,4 +1,30 @@
 package edu.wit.dcsn.comp2000.listapp;
 
-public class Player {
+public class Player
+{
+    public String name = null;
+
+    private int score = 0;
+
+    PlayerHand hand = new PlayerHand();
+
+    public void Player(String name)
+    {
+        this.name = name;
+    }
+
+    public void increaseScore()
+    {
+        this.score++;
+    }
+
+    public int getScore()
+    {
+        return this.score;
+    }
+
+    public void makeMove()
+    {
+
+    }
 }
--- src/edu/wit/dcsn/comp2000/listapp/PlayerHand.java
+++ src/edu/wit/dcsn/comp2000/listapp/PlayerHand.java
@@ -1,4 +1,65 @@
 package edu.wit.dcsn.comp2000.listapp;
 
-public class PlayerHand {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlayerHand extends PileOfCards{
+
+    ArrayList<Card> currentHand = new ArrayList();
+
+    int cardCap = 5;
+
+    public void PlayerHand()
+    {
+        for (int i = 0; i < 5; i++) {
+            currentHand.add(topCard);
+        }
+    }
+
+    public void drawCard()
+    {
+        if (currentHand.size() < cardCap)
+        {
+            currentHand.add(topCard);
+        }
+        else
+            {
+                System.out.println("Can't draw card! You already have 5!");
+            }
+    }
+
+    public Card releaseCard(Card card)
+    {
+        if (currentHand.contains(card))
+        {
+            currentHand.remove(card);
+            return card;
+        }
+        return null;
+    }
+
+    public void retrieveCard(PlayerHand selectedHand, Card card)
+    {
+        if (currentHand.contains(card) && selectedHand.currentHand.contains(card))
+        {
+            selectedHand.releaseCard(card);
+            currentHand.add(card);
+        }
+    }
+
+    public boolean combineCards()
+    {
+        Set<Card> dupes = new HashSet<>();
+        for (Card card : currentHand)
+        {
+            if (!dupes.add(card))
+            {
+                currentHand.remove(card);
+                currentHand.remove(card);
+                return true;
+            }
+        }
+        return false;
+    }
 }
